ef358d0b0446584c55bcb8cfd0397b487100ac7e
Adepitan Demilade <demi.adepitan@gmail.com>
2024-10-06 22:31:36 +0100

    Solving measures

--- next.config.mjs
+++ next.config.mjs
@@ -2,36 +2,39 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.pexels.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'www.washingtonpost.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'i.imgur.com',
-      },
-      // ... other image patterns you might have
-    ],
+      remotePatterns: [
+          {
+              protocol: 'https',
+              hostname: 'images.pexels.com',
+          },
+          {
+              protocol: 'https',
+              hostname: 'images.unsplash.com',
+          },
+          {
+              protocol: 'https',
+              hostname: 'www.washingtonpost.com',
+          },
+          {
+              protocol: 'https',
+              hostname: 'i.imgur.com',
+          },
+          // ... other image patterns you might have
+      ],
   },
+  webpack: (config, { isServer }) => {
+      if (!isServer) {
+          // Ignore these modules in client-side bundles
+          config.resolve.fallback = {
+              ...config.resolve.fallback,
+              net: false,
+              tls: false,
+              dns: false,
+              child_process: false,
+          };
+      }
+      return config;
+  },
+};
 
-  // Choose one of the following methods for CORS configuration:
-
-  // Method 1: Direct CORS Configuration (Next.js 13
-    // OR
-
-    // Method 2: Using `next-cors` Middleware (Next.js 12 and earlier)
-    // middleware: {
-    //   handler: require('./middleware'), // Replace with your middleware path
-    // },
-  };
-
-  export default nextConfig;
+export default nextConfig;
