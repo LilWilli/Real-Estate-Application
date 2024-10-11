3a0bad9f10c77eab55ee2f54e074110d00a35597
Adepitan Demilade <demi.adepitan@gmail.com>
2024-10-11 12:37:11 +0100

    Solving measures

--- pages/shop/EstateCategory.jsx
+++ pages/shop/EstateCategory.jsx
@@ -16,17 +16,22 @@ const EstateCategory = ({ filterItems, setProducts, menuItems, selectedCategory,
                 >
                     All
                 </button>
-                {menuItems.map((menuItem, index) => (
-                    <button 
-                        key={index} 
-                        className={`m-2 ${selectedCategory === menuItem ? "bg-warning" : ""}`} 
-                        onClick={() => {
-                            filterItems(menuItem); 
-                        }}
-                    >
-                        {menuItem}
-                    </button>
-                ))}
+                {/* Add a check to ensure menuItems is defined and is an array */}
+                {Array.isArray(menuItems) && menuItems.length > 0 ? (
+                    menuItems.map((menuItem, index) => (
+                        <button 
+                            key={index} 
+                            className={`m-2 ${selectedCategory === menuItem ? "bg-warning" : ""}`} 
+                            onClick={() => {
+                                filterItems(menuItem); 
+                            }}
+                        >
+                            {menuItem}
+                        </button>
+                    ))
+                ) : (
+                    <p>No categories available.</p> // Optional: Handle the case when there are no menu items
+                )}
             </div>
         </>
     );
